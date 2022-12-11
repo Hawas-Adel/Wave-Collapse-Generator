c0250b97497fcf3d72f03edb11908c9221b9f551
HawasNomad <36897875+Hawas-Adel@users.noreply.github.com>
2022-12-11 23:13:02 +0200

    revert to working state

--- Assets/Wave Function Collapse Generator/_Scripts/WaveFunctionCell.cs
+++ Assets/Wave Function Collapse Generator/_Scripts/WaveFunctionCell.cs
@@ -6,20 +6,13 @@ public class WaveFunctionCell : MonoBehaviour
 {
 	#region Borders & Neighbors
 	private enum NeighborBorderState { DontCare, Allow, DontAllow }
-	[System.Serializable]
-	private struct NeighborBordersState
-	{
-		public NeighborBorderState NeighborState;
-		public NeighborBorderState RightNeighborState;
-		public NeighborBorderState LeftNeighborState;
-	}
 
-	[SerializeField] private NeighborBordersState ForwardNeighborState;
-	[SerializeField] private NeighborBordersState BackNeighborsState;
-	[SerializeField] private NeighborBordersState RightNeighborsState;
-	[SerializeField] private NeighborBordersState LeftNeighborsState;
+	[SerializeField] private NeighborBorderState ForwardNeighborState;
+	[SerializeField] private NeighborBorderState BackNeighborsState;
+	[SerializeField] private NeighborBorderState RightNeighborsState;
+	[SerializeField] private NeighborBorderState LeftNeighborsState;
 
-	private NeighborBordersState GetNeighborBordersState(Vector3 localDirection)
+	private NeighborBorderState GetNeighborBordersState(Vector3 localDirection)
 	{
 		localDirection.Normalize();
 		if (localDirection == Vector3.forward)
@@ -45,68 +38,22 @@ public class WaveFunctionCell : MonoBehaviour
 		throw new System.ArgumentException(localDirection.ToString());
 	}
 
-	private (NeighborBorderState forward, NeighborBorderState Right, NeighborBorderState Left) GetBordersState(Vector3 localDirection)
-	{
-		localDirection.Normalize();
-		if (localDirection == Vector3.forward)
-		{
-			return (ForwardNeighborState.NeighborState, LeftNeighborsState.NeighborState, RightNeighborsState.NeighborState);
-		}
-
-		if (localDirection == Vector3.back)
-		{
-			return (BackNeighborsState.NeighborState, RightNeighborsState.NeighborState, LeftNeighborsState.NeighborState);
-		}
-
-		if (localDirection == Vector3.right)
-		{
-			return (RightNeighborsState.NeighborState, ForwardNeighborState.NeighborState, BackNeighborsState.NeighborState); ;
-		}
-
-		if (localDirection == Vector3.left)
-		{
-			return (LeftNeighborsState.NeighborState, BackNeighborsState.NeighborState, ForwardNeighborState.NeighborState);
-		}
-
-		throw new System.ArgumentException(localDirection.ToString());
-	}
-
 	public bool DoesNeighborFit(float currentRotation, WaveFunctionCell neighbor, float neighborRotation, Vector3 directionToNeighbor)
 	{
-		NeighborBordersState bordersState = GetNeighborBordersState(Quaternion.Euler(0f, -currentRotation, 0f) * directionToNeighbor);
+		var bordersState = GetNeighborBordersState(Quaternion.Euler(0f, -currentRotation, 0f) * directionToNeighbor);
 		if (!neighbor)
 		{
-			return bordersState.NeighborState != NeighborBorderState.DontAllow;
+			return bordersState != NeighborBorderState.DontAllow;
 		}
 
-		(NeighborBorderState forward, NeighborBorderState Right, NeighborBorderState Left) neighborBordersState = neighbor.GetBordersState(Quaternion.Euler(0f, -neighborRotation, 0f) * -directionToNeighbor);
-		//Debug.Log($"{neighbor.name} ({directionToNeighbor}) : {neighborBordersState.forward}, {neighborBordersState.Right}, {neighborBordersState.Left} ({neighborRotation})");
-		if (!AreBorderStatesAValidMatch(bordersState.NeighborState, neighborBordersState.forward))
-		{
-			return false;
-		}
-
-		if (!AreBorderStatesAValidMatch(bordersState.RightNeighborState, neighborBordersState.Right))
-		{
-			return false;
-		}
-
-		if (!AreBorderStatesAValidMatch(bordersState.LeftNeighborState, neighborBordersState.Left))
-		{
-			return false;
-		}
-
-		return true;
-	}
-
-	private bool AreBorderStatesAValidMatch(NeighborBorderState source, NeighborBorderState neighbor)
-	{
-		if (source == NeighborBorderState.DontCare)
+		if (bordersState == NeighborBorderState.DontCare)
 		{
 			return true;
 		}
 
-		return source == neighbor;
+		var neighborBordersState = neighbor.GetNeighborBordersState(Quaternion.Euler(0f, -neighborRotation, 0f) * -directionToNeighbor);
+
+		return bordersState == neighborBordersState;
 	}
 	#endregion
 
@@ -177,25 +124,17 @@ public class WaveFunctionCell : MonoBehaviour
 	private void OnDrawGizmosSelected()
 	{
 		Gizmos.matrix = transform.localToWorldMatrix;
-		DrawNeighborGizmo(Vector3.forward, ForwardNeighborState);
-		DrawNeighborGizmo(Vector3.back, BackNeighborsState);
-		DrawNeighborGizmo(Vector3.right, RightNeighborsState);
-		DrawNeighborGizmo(Vector3.left, LeftNeighborsState);
+		DrawBorderGizmo(Vector3.forward, GetBorderStateColor(ForwardNeighborState));
+		DrawBorderGizmo(Vector3.back, GetBorderStateColor(BackNeighborsState));
+		DrawBorderGizmo(Vector3.right, GetBorderStateColor(RightNeighborsState));
+		DrawBorderGizmo(Vector3.left, GetBorderStateColor(LeftNeighborsState));
 	}
 
-	private void DrawNeighborGizmo(Vector3 direction, NeighborBordersState neighborState)
-	{
-		Vector3 neighborCenter = 2f * BorderGizmoCenterOffset * direction.normalized;
-		DrawBorderGizmo(neighborCenter, -direction, GetBorderStateColor(neighborState.NeighborState));
-		DrawBorderGizmo(neighborCenter, Vector3.Cross(direction, Vector3.down), GetBorderStateColor(neighborState.RightNeighborState));
-		DrawBorderGizmo(neighborCenter, Vector3.Cross(direction, Vector3.up), GetBorderStateColor(neighborState.LeftNeighborState));
-	}
-
-	private void DrawBorderGizmo(Vector3 center, Vector3 direction, Color color)
+	private void DrawBorderGizmo(Vector3 direction, Color color)
 	{
 		Gizmos.color = color;
 		Vector3 size = Quaternion.LookRotation(direction, Vector3.up) * BorderGizmoSize;
-		Gizmos.DrawCube(center + (direction.normalized * BorderGizmoCenterOffset), size);
+		Gizmos.DrawCube(direction.normalized * BorderGizmoCenterOffset, size);
 	}
 
 	private Color GetBorderStateColor(NeighborBorderState state)
